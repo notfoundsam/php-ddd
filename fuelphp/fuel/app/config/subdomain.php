4e8f287567137e149c66e5c1643d77998116792d
notfoundsam <notfoundsam@gmail.com>
2025-06-28 20:31:07 +0900

    Restrict access to the modules directory by redirecting requests to a 404 page

--- fuelphp/fuel/app/config/subdomain.php
+++ fuelphp/fuel/app/config/subdomain.php
@@ -1,6 +1,12 @@
 <?php
 
 $host = $_SERVER['HTTP_HOST'] ?? '';
+$uri = $_SERVER['REQUEST_URI'] ?? '/';
+
+// Do not allow access to the modules directory directly
+if (preg_match('/^admin/', ltrim($uri, '/'))) {
+    $_SERVER['REQUEST_URI'] = str_replace(['admin'], '_404_', $uri);
+}
 
 if (preg_match('/^admin\./', $host)) {
     $_SERVER['REQUEST_URI'] = '/admin' . $_SERVER['REQUEST_URI'];
