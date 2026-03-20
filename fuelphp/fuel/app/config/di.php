671c13dcc9b5db7f0a5c486775883101a19c264c
notfoundsam <notfoundsam@gmail.com>
2026-03-20 21:12:41 +0900

    Add Redis client and cache factory to dependency injection container

--- fuelphp/fuel/app/config/di.php
+++ fuelphp/fuel/app/config/di.php
@@ -2,9 +2,13 @@
 
 use DI\ContainerBuilder;
 use Fuel\Core\Fuel;
+use SharedKernel\Domain\Cache\CacheInterface;
 use SharedKernel\Domain\Environment;
 use SharedKernel\Domain\Logger\LoggerInterface;
+use SharedKernel\Domain\Redis\RedisClientInterface;
+use SharedKernel\Infrastructure\Cache\CacheFactory;
 use SharedKernel\Infrastructure\Logger\LoggerFactory;
+use SharedKernel\Infrastructure\Redis\RedisClientFactory;
 
 $containerBuilder = new ContainerBuilder();
 $repositories = require APPPATH . 'config/repositories.php';
@@ -17,6 +21,8 @@ if (Fuel::$env !== Fuel::DEVELOPMENT) {
 $containerBuilder->addDefinitions(array_merge([
     Environment::class => DI\create(Environment::class)->constructor(Fuel::$env, getenv('TEST_ENV_ID') ?: null),
     LoggerInterface::class => DI\factory(LoggerFactory::class),
+    RedisClientInterface::class => DI\factory(RedisClientFactory::class),
+    CacheInterface::class => DI\factory(CacheFactory::class),
 ]), $repositories);
 
 return $containerBuilder->build();
