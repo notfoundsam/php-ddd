7d9b1ba48a00f268be683e18e7a5cd0d4aeae11f
notfoundsam <notfoundsam@gmail.com>
2026-03-22 20:57:48 +0900

    Configure DI container for CDN and storage services

--- fuelphp/fuel/app/config/di.php
+++ fuelphp/fuel/app/config/di.php
@@ -2,13 +2,17 @@
 
 use DI\ContainerBuilder;
 use Fuel\Core\Fuel;
+use Psr\Container\ContainerInterface;
 use SharedKernel\Domain\Cache\CacheInterface;
 use SharedKernel\Domain\Environment;
 use SharedKernel\Domain\Logger\LoggerInterface;
 use SharedKernel\Domain\Redis\RedisClientInterface;
+use SharedKernel\Domain\Storage\StorageInterface;
 use SharedKernel\Infrastructure\Cache\CacheFactory;
 use SharedKernel\Infrastructure\Logger\LoggerFactory;
 use SharedKernel\Infrastructure\Redis\RedisClientFactory;
+use SharedKernel\Infrastructure\Storage\CdnUrlResolver;
+use SharedKernel\Infrastructure\Storage\StorageFactory;
 
 $containerBuilder = new ContainerBuilder();
 $repositories = require APPPATH . 'config/repositories.php';
@@ -23,6 +27,13 @@ $containerBuilder->addDefinitions(array_merge([
     LoggerInterface::class => DI\factory(LoggerFactory::class),
     RedisClientInterface::class => DI\factory(RedisClientFactory::class),
     CacheInterface::class => DI\factory(CacheFactory::class),
+    CdnUrlResolver::class => DI\create(CdnUrlResolver::class)->constructor([
+        'public/images/' => getenv('CDN_IMAGES_URL') ?: '',
+    ]),
+    StorageFactory::class => DI\autowire(StorageFactory::class),
+    StorageInterface::class => DI\factory(function(ContainerInterface $c) {
+        return $c->get(StorageFactory::class)->create();
+    }),
 ]), $repositories);
 
 return $containerBuilder->build();
