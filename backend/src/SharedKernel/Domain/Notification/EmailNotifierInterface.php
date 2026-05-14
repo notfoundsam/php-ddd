3ca356e59bcf5d11e99b54bf1ad807cedb43dfea
notfoundsam <notfoundsam@gmail.com>
2026-05-14 17:47:32 +0900

    refactor: remove old classes of notification module

--- backend/src/SharedKernel/Domain/Notification/EmailNotifierInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace SharedKernel\Domain\Notification;
-
-interface EmailNotifierInterface extends NotifierInterface
-{
-}
--- backend/src/SharedKernel/Domain/Notification/SmsNotifierInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace SharedKernel\Domain\Notification;
-
-interface SmsNotifierInterface extends NotifierInterface
-{
-}
